81620e0ca90ff481ab1747b2c7993a18a4265409
asadbektuit <davlatovasadbek1@gmail.com>
2022-10-04 09:06:05 +0500

    Initial commit

---
+++ src/main/java/com/example/qabul/QabulApplication.java
@@ -0,0 +1,13 @@
+package com.example.qabul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QabulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QabulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qabul/QabulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qabul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QabulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
