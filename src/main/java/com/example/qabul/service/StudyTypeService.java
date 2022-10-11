73245dd13c82d679c6ecdfda9aeecbac82e60a6c
asadbektuit <davlatovasadbek1@gmail.com>
2022-10-11 18:17:32 +0500

    goo

--- src/main/java/com/example/qabul/service/StudyTypeService.java
+++ src/main/java/com/example/qabul/service/StudyTypeService.java
@@ -1,24 +1,56 @@
 package com.example.qabul.service;
 
+import com.example.qabul.dto.DegreeDto;
+import com.example.qabul.dto.SportTypeDto;
 import com.example.qabul.dto.StudyTypeDto;
+import com.example.qabul.entity.Degree;
+import com.example.qabul.entity.StudyType;
+import com.example.qabul.exception.BadRequest;
+import com.example.qabul.repository.DegreeRepository;
+import com.example.qabul.repository.StudyTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
-public class StudyTypeService
-{
+public class StudyTypeService {
+
+    @Autowired
+    private StudyTypeRepository studyTypeRepository;
+
     public StudyTypeDto get(Integer id) {
-        return null;
+        StudyType studyType = getEntity(id);
+        StudyTypeDto dto = new StudyTypeDto();
+        dto.setName(studyType.getName());;
+        return dto;
     }
-
     public StudyTypeDto create(StudyTypeDto dto) {
-        return null;
+        StudyType studyType = new StudyType();
+        studyType.setName(dto.getName());
+        studyType.setCreatedAt(LocalDateTime.now());
+        studyTypeRepository.save(studyType);
+        return dto;
     }
-
     public boolean update(Integer id, StudyTypeDto dto) {
-        return false;
+        StudyType studyType = getEntity(id);
+        studyType.setName(dto.getName());
+        studyType.setUpdatedAt(LocalDateTime.now());
+        studyTypeRepository.save(studyType);
+        return true;
     }
-
     public boolean delete(Integer id) {
-        return false;
+        StudyType studyType = getEntity(id);
+        studyType.setDeletedAt(LocalDateTime.now());
+        studyTypeRepository.save(studyType);
+        return true;
+    }
+    private StudyType getEntity(Integer id) {
+        Optional<StudyType> optional = studyTypeRepository.findByIdAndDeletedAtIsNull(id);
+        if (optional.isEmpty()){
+            throw new BadRequest("Study type not found");
+        }
+        return optional.get();
     }
 }
--- src/main/java/com/example/qabul/service/UniversityNameService.java
+++ src/main/java/com/example/qabul/service/UniversityNameService.java
@@ -1,23 +1,51 @@
 package com.example.qabul.service;
 
 import com.example.qabul.dto.UniversityNameDto;
+import com.example.qabul.entity.UniversityName;
+import com.example.qabul.exception.BadRequest;
+import com.example.qabul.repository.UniversityNameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class UniversityNameService {
+    @Autowired
+    private UniversityNameRepository universityNameRepository;
+
     public UniversityNameDto get(Integer id) {
-        return null;
+        UniversityName universityName = getEntity(id);
+        UniversityNameDto dto = new UniversityNameDto();
+        dto.setName(universityName.getName());;
+        return dto;
     }
-
     public UniversityNameDto create(UniversityNameDto dto) {
-        return null;
+        UniversityName universityName = new UniversityName();
+        universityName.setName(dto.getName());
+        universityName.setCreatedAt(LocalDateTime.now());
+        universityNameRepository.save(universityName);
+        return dto;
     }
-
     public boolean update(Integer id, UniversityNameDto dto) {
-        return false;
+        UniversityName universityName = getEntity(id);
+        universityName.setName(dto.getName());
+        universityName.setUpdatedAt(LocalDateTime.now());
+        universityNameRepository.save(universityName);
+        return true;
     }
-
     public boolean delete(Integer id) {
-        return false;
+        UniversityName universityName = getEntity(id);
+        universityName.setDeletedAt(LocalDateTime.now());
+        universityNameRepository.save(universityName);
+        return true;
+    }
+    private UniversityName getEntity(Integer id) {
+        Optional<UniversityName> optional = universityNameRepository.findByIdAndDeletedAtIsNull(id);
+        if (optional.isEmpty()){
+            throw new BadRequest("University name not found");
+        }
+        return optional.get();
     }
 }
