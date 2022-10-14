e415a50554705b269914009b72b7fcb1e8210ca5
FarruxTUIT <odamboyevfarrux2244@gmail.com>
2022-10-14 18:06:11 +0800

    777

--- src/main/java/com/example/qabul/dto/StudyPlaceDto.java
+++ src/main/java/com/example/qabul/dto/StudyPlaceDto.java
@@ -4,16 +4,20 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class StudyPlaceDto {
-
+    @NotNull
     private Integer id;
     private String name;
+    @NotNull
     private LocalDateTime createdAt;
+    @NotNull
     private LocalDateTime updatedAt;
+    @NotNull
     private LocalDateTime deletedAt;
 }
